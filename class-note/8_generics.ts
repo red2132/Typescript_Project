f469f65f8fd4aa002108e97dfffaee42e416aaac
hyejin Han <86100654+red2132@users.noreply.github.com>
2024-07-12 15:25:27 +0900

    제네릭 학습2

--- class-note/8_generics.ts
+++ class-note/8_generics.ts
@@ -25,3 +25,53 @@ function logText3<T>(text: T): T {
 
 // 호출할 때 구체적인 타입 선언 가능
 logText3<string>('하이')
+
+// 인터페이스에 제네릭 선언하는 법
+interface Dropdown {
+    value: string
+    selected: boolean
+}
+
+const obj: Dropdown = {value: 'abc', selected: false}
+
+interface Dropdown2<T> {
+    value: T
+    selected: boolean
+}
+const obj2: Dropdown2<string> = {value: 'abc', selected: false}
+const obj3: Dropdown2<number> = {value: 11, selected: false}
+
+// 제네릭의 타입 제한(배열로 타입 제한)
+function logTextLength<T>(text: T[]) : T[] {
+    text.forEach(function(text) {
+        console.log(text)
+    })
+    return text
+}
+
+logTextLength<string>(['hi', 'abc'])
+
+// 제네릭 타입 제한2 - 정의된 타입 이용하기
+interface LengthType {
+    length: number
+}
+
+function logTextLength2<T extends LengthType>(text: T): T {
+    // Length 타입을 상속받아 length 정의
+    text.length
+    return text
+}
+
+// 제네릭 타입 제한3 - key of
+interface ShoppingItem {
+    name: string
+    price: number
+    stock: number
+}
+// ShoppingItem에 있는 키값만 들어갈 수 있음
+function getShoppingItemOption<T extends keyof ShoppingItem>(itemOption: T): T {
+    return itemOption
+}
+
+getShoppingItemOption("name")
+getShoppingItemOption("price")
